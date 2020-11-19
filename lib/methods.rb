a442704db1c812fe85daa8f01033bebfdd3d5f4f
jane baik <janebaik@berkeley.edu>
2020-11-19 11:34:19 -0800

    finished two_sum

--- lib/methods.rb
+++ lib/methods.rb
@@ -1,17 +1,26 @@
 
 
 def remove_dups(arr)
+  raise ArgumentError if !arr.is_a?(Array)
   output = []
+  arr.each do |el|
+    output << el if !output.include?(el)
+  end
+  output
+end
 
-  arr.each_with_index do |ele, idx|
-    arr.each_with_index do |ele2, idx2|
-      if idx2 > idx
-        if idx == idx2
-          arr.delete_at(idx)
+class Array 
+
+  def two_sum
+    new_array = []
+    self.each_with_index do |e1, i1|
+      self.each_with_index do |e2, i2|
+        if (i2 > i1) && (e1 + e2 == 0)
+          new_array << [i1,i2]
         end
       end
     end
+    new_array
   end
 
-  arr
 end
--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -2,13 +2,35 @@ require "rspec"
 require "methods"
 
 describe "#remove_dups" do
-  # it "should take in only an Array" do
-  #   expect {  }
-  # end
 
   it "returns unique elements in the order in which they first appeared" do
     arr = [1, 2, 1, 3, 3]
     expect(remove_dups(arr)).to eq([1,2,3])
+  end
+
+  it "should only accept an array" do 
+    expect { remove_dups("bananna") }.to raise_error(ArgumentError)
+  end
+
+  it "should return a new array" do 
+    arr = [1, 2, 1, 3, 3]
+    expect(arr).not_to be(remove_dups(arr))
+  end
+
+end
+
+describe "#two_sum" do 
+
+subject(:arr) {arr=[-1, 0, 2, -2, 1]}
+
+  it "should return an array of index of elements that sum to zero " do 
+    expect(arr.two_sum).to eq([[0,4],[2,3]])
+  end
+
+  it "output should be sorted dictionary:wise" do 
+    expect(arr.two_sum[0][0]).to be < (arr.two_sum[0][1])
+    expect(arr.two_sum[0][0]).to be < (arr.two_sum[1][0])
+
   end
 
 
